b373ee2c1342c1da417cb5aa961da9f08310657d
Rahin Arefin Ahmed <rahin520@gmail.com>
2026-09-01 13:07:10 +0600

    Add capacitor configuration and finalize all auth updates

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.publicsheba.dhk',
+  appName: 'Public Sheba DHK',
+  webDir: 'out'
+};
+
+export default config;
